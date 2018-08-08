59e8f14be6333236647a81b076c4dae5c44a5955
github username <github email address>
2018-08-08 12:08:12 -0700

    Removed comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # logs the user in -
-    #finds the user by params[:id]
-    #if found, authenticates the user and logs them in (session[:id] = user.id)
-    #if not found, redirects the user to login page
-    # raise params.inspect
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
