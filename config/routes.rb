b6ff3ae09d268199332adee62d4ce77746250f3e
github username <github email address>
2018-08-08 12:00:44 -0700

    Drew custom routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#welcome'
-
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  delete '/signout' => 'sessions#destroy'
+  
   resources :users do
     resources :boards
   end
 
   resources :outfits
-  resources :sessions, only: [:new, :create, :destroy]
+
 
 end
