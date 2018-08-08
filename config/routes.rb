a579158bfaee0d07b26205547f41ee7e664f17d9
github username <github email address>
2018-08-07 23:14:08 -0700

    Added resources for users, boards and outfits

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#welcome'
+
+  resources :users do
+    resources :boards
+  end
+
+  resources :outfits
+
 end
