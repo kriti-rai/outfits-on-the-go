54b1960f1bb6370f22b6a6931692a8f1d78277e4
github username <github email address>
2018-08-08 01:25:27 -0700

    Added actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    # logs the user in -
+    #finds the user by params[:id]
+    #if found, authenticates the user and logs them in (session[:id] = user.id)
+    #if not found, redirects the user to login page
+
+  end
+
+  def destroy
+    session.delete :id
+    session[:id] = nil
+    redirect_to root_url
+  end
+
 end
