49759133e8a0e70f9c06a675fdc227cf569ae405
github username <github email address>
2018-08-07 23:16:43 -0700

    Added methods for routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:create, :edit, :update]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def find_user
+      @user = User.find_by(params[:id])
+    end
 end
