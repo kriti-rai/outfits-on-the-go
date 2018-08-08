dccd46456f0b311dd238578a3dcb1997d72ebb10
github username <github email address>
2018-08-08 15:03:42 -0700

    Added code to create, show and destroy actions

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,16 +1,47 @@
 class BoardsController < ApplicationController
+  before_action :require_login
+
   def new
+    @board = Board.new
+    # @user = current_user
+    @board.user = current_user
   end
 
   def create
+    #board needs to know that it belongs to a user
+    #board needs to know if it has any outfits (edit?)
+    #only allowed to create if a user is logged in
+    @board = Board.create(name: params[:board][:name], user_id: current_user.id)
+    # @user = current_user
+    # binding.pry
+    redirect_to user_board_path(current_user, @board)
   end
 
   def edit
+    #only allowed by the user to whom the board belongs
   end
 
   def update
   end
 
-  def destroy
+  def show
+    @board = Board.find_by(id: params[:id])
+    @user = @board.user
   end
+
+  def destroy
+    #only allowed by the user to whom the board belongs
+    @board = Board.find(params[:id]).destroy
+      redirect_to user_path(@board.user)
+  end
+
+  private
+    def board_params
+      params.require(:board).permit(:name, :user_id)
+    end
+
+    def require_login
+      redirect_to root_url unless logged_in?
+    end
+
 end
