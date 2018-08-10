81a55aa8c56bc755d46090fe2d939b581a7bb67d
github username <github email address>
2018-08-09 20:06:51 -0700

    Added img column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_184525) do
+ActiveRecord::Schema.define(version: 2018_08_10_030056) do
 
   create_table "boards", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_08_09_184525) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "uid"
+    t.string "img"
   end
 
 end
