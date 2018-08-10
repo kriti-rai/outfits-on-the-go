7fcf3d7636f1c50b9e4e9c58ed0b22dfcc59ddd5
github username <github email address>
2018-08-09 20:06:33 -0700

    Added option to login via facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to root_url
+    if auth
+      raise params.inspect
+
+      @user = User.find_or_create_by(auth)
+      binding.pry
+      session[:user_id] = @user.id
+      redirect_to @user
     else
-      flash[:error] = "The username and password combination does not match our records."
-      redirect_to signin_path
+      @user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to root_url
+      else
+        flash[:error] = "The username and password combination does not match our records."
+        redirect_to signin_path
+      end
     end
   end
 
@@ -18,5 +27,10 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url
   end
+
+  private
+    def auth
+      request.env['omniauth.auth']
+    end
 
 end
