3107401c2b759dbe09b8f635c4a01c25449afa88
github username <github email address>
2018-08-10 22:57:24 -0700

    Comments and reformatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root url
   root to: 'sessions#new'
+
+  #login and logout
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
-  get '/auth/facebook/callback' => 'sessions#create'
-  get 'auth/failure', to: redirect('/')
   delete '/signout' => 'sessions#destroy'
 
+  #login via Facebook
+  get '/auth/facebook/callback' => 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+
+
+  #routes for users, boards, outfits
   resources :users, shallow: true do
-    resources :boards, shallow: true do
+    resources :boards
       resources :outfits
-    end
   end
 
+  #once user is logged in show 'welcome'
   get '/welcome' => 'welcome#welcome'
 
 end
