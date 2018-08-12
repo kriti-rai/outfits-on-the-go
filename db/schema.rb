acbc31c6786f3c5bdb81b10c73a0f9a8241380b0
github username <github email address>
2018-08-11 23:35:04 -0700

    Added tags to outfits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_10_035909) do
+ActiveRecord::Schema.define(version: 2018_08_12_054146) do
 
   create_table "boards", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_08_10_035909) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.text "tags"
   end
 
   create_table "users", force: :cascade do |t|
