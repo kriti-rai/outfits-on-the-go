c5e94cfc8d2543debb79f81ff788f8a5bd9c1852
github username <github email address>
2018-08-12 15:22:14 -0700

    Added a scope method and #created_date instance method

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,5 +2,10 @@ class Board < ApplicationRecord
   belongs_to :user
   has_many :outfits, dependent: :destroy
   accepts_nested_attributes_for :outfits
+  scope :newest_to_oldest, -> {order("boards.updated_at DESC")}
+
+  def created_date
+    self.created_at.strftime("%b %e, %l:%M %p")
+  end
 
 end
