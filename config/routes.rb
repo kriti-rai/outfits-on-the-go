ae5611bf077e914ad81becfda4d95e8e8330839d
github username <github email address>
2018-08-12 17:54:08 -0700

    Added /boards/feed; clean-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #root url
+  # get 'hashtags/',         to: 'hashtags#index',     as: :hashtags
+  # get 'hashtags/:hashtag', to: 'hashtags#show',      as: :hashtag
+
+  get '/boards/feed' => 'boards#feed', as: :feed
+  post '/boards/feed' => 'boards#feed'
+
   root to: 'sessions#new'
 
-  #login and logout
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   delete '/signout' => 'sessions#destroy'
 
-  #login via Facebook
   get '/auth/facebook/callback' => 'sessions#create'
   get 'auth/failure', to: redirect('/')
 
-
-  #routes for users, boards, outfits
   resources :users do
     resources :boards , shallow: true do
       resources :outfits, shallow: true
+    end
   end
-end
-
-  #once user is logged in show 'welcome'
-  get '/welcome' => 'welcome#welcome'
 
 end
