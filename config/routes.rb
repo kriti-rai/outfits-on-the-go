572e7aaffbf3b1c277d5751d4ce7ecbe74b6ac28
github username <github email address>
2018-08-13 23:48:52 -0700

    Added a route for hashtag

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get 'hashtags/',         to: 'hashtags#index',     as: :hashtags
-  # get 'hashtags/:hashtag', to: 'hashtags#show',      as: :hashtag
+  # get '/hashtags',         to: 'hashtags#index',     as: :hashtags
+  get '/hashtags/:hashtag', to: 'hashtags#show',      as: :hashtag
 
   get '/boards/feed' => 'boards#feed', as: :feed
   post '/boards/feed' => 'boards#feed'
