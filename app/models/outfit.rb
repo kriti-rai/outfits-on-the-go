618872a6aafaf3bb33e6a0e7703d19a2a05add0c
github username <github email address>
2018-08-16 12:12:10 -0700

    Removed simple_hashtag features from master and moved it to branch Hashtags

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,17 +1,7 @@
 class Outfit < ApplicationRecord
   mount_uploader :image, OutfitImageUploader
-  include SimpleHashtag::Hashtaggable
-  hashtaggable_attribute :tags
-
   belongs_to :board
   belongs_to :user
 
   validates_presence_of :image
-
-  def hashtag_array
-    if self.tags
-      tags.split("#")
-    end
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '/hashtags',         to: 'hashtags#index',     as: :hashtags
-  get '/hashtags/:hashtag', to: 'hashtags#show',      as: :hashtag
+  root to: 'sessions#new'
 
   get '/boards/feed' => 'boards#feed', as: :feed
   post '/boards/feed' => 'boards#feed'
-
-  root to: 'sessions#new'
 
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
