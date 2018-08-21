3f70a1d199aa9ba27e40a74beede39c3e4ed332a
github username <github email address>
2018-08-21 14:38:01 -0700

    Commented out callbacks

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -9,24 +9,24 @@ class Outfit < ApplicationRecord
   has_many :tags, through: :outfits_tags
 
   validates_presence_of :image
-
-  after_create do
-    outfit = Outfit.find_by(id: self.id)
-    hashtags = self.hashtags.scan(/#\w+/)
-    hashtags.map do |hashtag|
-      tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
-      outfit.tags << tag
-    end
-  end
-
-  before_update do
-    outfit = Outfit.find_by(id: self.id)
-    outfit.tags.clear
-    hashtags = self.tags.scan(/#\w+/)
-    hashtags.map do |hashtag|
-      tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
-      post.tags << tag
-    end
-  end
+  #
+  # after_create do
+  #   outfit = Outfit.find_by(id: self.id)
+  #   hashtags = outfit.tags.scan(/#\w+/)
+  #   hashtags.map do |hashtag|
+  #     tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
+  #     outfit.tags << tag
+  #   end
+  # end
+  #
+  # before_update do
+  #   outfit = Outfit.find_by(id: self.id)
+  #   outfit.tags.clear
+  #   hashtags = self.tags.scan(/#\w+/)
+  #   hashtags.map do |hashtag|
+  #     tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
+  #     post.tags << tag
+  #   end
+  # end
 
 end
