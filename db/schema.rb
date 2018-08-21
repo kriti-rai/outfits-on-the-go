5c38a93bf4fb61104825750177218bcbb9d280e0
github username <github email address>
2018-08-21 14:39:44 -0700

    Fixed the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_054554) do
+ActiveRecord::Schema.define(version: 2018_08_21_212108) do
 
   create_table "boards", force: :cascade do |t|
     t.string "name"
@@ -26,30 +26,12 @@ ActiveRecord::Schema.define(version: 2018_08_16_054554) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
-    t.text "hashtags"
+    t.text "tags"
   end
 
   create_table "outfits_tags", force: :cascade do |t|
     t.integer "outfit_id"
     t.integer "tag_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "simple_hashtag_hashtaggings", force: :cascade do |t|
-    t.integer "hashtag_id"
-    t.string "hashtaggable_type"
-    t.integer "hashtaggable_id"
-    t.index ["hashtag_id"], name: "index_hashtaggings_hashtag"
-    t.index ["hashtaggable_id", "hashtaggable_type"], name: "index_hashtaggings_hashtaggable_id_hashtaggable_type"
-    t.index ["hashtaggable_type", "hashtaggable_id"], name: "index_hashtaggings_hashtaggable"
-  end
-
-  create_table "simple_hashtag_hashtags", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_simple_hashtag_hashtags_on_name"
   end
 
   create_table "tags", force: :cascade do |t|
