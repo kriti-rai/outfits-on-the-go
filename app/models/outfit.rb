c5fd07dd7e7cde34fc8ca8687686c357ceb5fd0b
github username <github email address>
2018-08-22 00:17:22 -0700

    Added depenedent:destroy

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -3,27 +3,28 @@ class Outfit < ApplicationRecord
   belongs_to :board
   belongs_to :user
   has_many :outfits_tags
-  has_many :tags, through: :outfits_tags
+  has_many :tags, through: :outfits_tags, dependent: :destroy
 
   validates_presence_of :image
 
   after_create do
     outfit = Outfit.find_by(id: self.id)
-    add_tags(outfit)
-  end
-
-  before_update do
-    outfit = Outfit.find_by(id: self.id)
-    outfit.tags.clear
-    add_tags(outfit)
-  end
-
-  def add_tags(outfit)
     hashtags = outfit.hashtags.scan(/#\w+/)
     hashtags.uniq.map do |hashtag|
       tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
       outfit.tags << tag
     end
   end
+
+  before_update do
+    outfit = Outfit.find_by(id: self.id)
+    outfit.tags.clear
+    hashtags = self.hashtags.scan(/#\w+/)
+    hashtags.uniq.map do |hashtag|
+      tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
+      outfit.tags << tag
+    end
+  end
+
 
 end
