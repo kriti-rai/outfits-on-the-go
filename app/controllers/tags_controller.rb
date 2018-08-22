b7cbbb5be2da2e52352544f26472037b3f1c8051
github username <github email address>
2018-08-21 22:02:29 -0700

    Edited #show and clean up

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,17 +1,13 @@
 class TagsController < ApplicationController
 
   def index
-    # @hashtags = SimpleHashtag::Hashtag.all
     @hashtags = Tag.all
   end
 
   def show
-    binding.pry
-    # @hashtag = SimpleHashtag::Hashtag.find_by_name(params[:hashtag])
-    # @hashtagged = @hashtag.hashtaggables if @hashtag
-    tag = Tag.find_by(name: params[:hashtag])
-    @outfits = tag.outfits
-    # binding.pry
+    if tag = Tag.find_by(name: params[:hashtag])
+      @outfits = tag.outfits
+    end
   end
 
 end
